d5f2088c1f25c69c07d633afcdbc5f8e33c3bcd1
Ango <ango.gomes@gmail.com>
2020-04-28 10:45:02 +0200

    django project creation, product app with model created

---
+++ product/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Product(models.Model):
+    code: models.CharField = models.CharField(
+        primary_key=True, max_length=55, blank=False, unique=True
+    )
+    name: models.CharField = models.CharField(max_length=255, blank=False)
+    nutriscore_grade: models.CharField = models.CharField(max_length=1, blank=False)
+    url: models.CharField = models.CharField(max_length=255, blank=False)
+    image: models.URLField = models.URLField(max_length=255, blank=False)
+    nutriscore_image: models.URLField = models.URLField(max_length=255, blank=False)
+    nutrient_levels: models.URLField = models.URLField(max_length=255, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name: models.CharField = models.CharField(max_length=255, blank=False, unique=True)
+    products: models.ManyToManyField = models.ManyToManyField(Product)
