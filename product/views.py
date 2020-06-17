66d865d2622cde277764b9fd9e4cdabf0405d2a5
Ango <ango.gomes@gmail.com>
2020-06-17 10:04:16 +0200

    updated templates directory structure

--- product/views.py
+++ product/views.py
@@ -3,15 +3,15 @@ from .models import Product, Category
 
 # Create your views here.
 def home(request):
-    return render(request, "search.html")
+    return render(request, "product/search.html")
 
 
 def detail(request, code_product):
     product = Product.objects.get(code=code_product)
-    return render(request, "detail.html", {"product": product})
+    return render(request, "product/detail.html", {"product": product})
 
 
 def index(request):
     category = Category.objects.get(name=request.GET.get("category"))
     products = category.products.all()
-    return render(request, "index.html", {"products": products})
+    return render(request, "product/index.html", {"products": products})
