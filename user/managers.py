0e615087b6c5d599076c7de8cd8effa39688f946
Ango <ango.gomes@gmail.com>
2020-06-21 17:46:00 +0200

    Added some control on user/superuser creation

--- user/managers.py
+++ user/managers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.base_user import BaseUserManager
 
+
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
@@ -14,11 +15,19 @@ class UserManager(BaseUserManager):
         return user
 
     def create_user(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_superuser', False)
-        
+        extra_fields.setdefault("is_superuser", False)
+
+        if not email:
+            raise ValueError("The given email must be set.")
+
         return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if not email:
+            raise ValueError("The given email must be set.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
 
         return self._create_user(email, password, **extra_fields)
--- user/tests/unit/test_managers.py
+++ user/tests/unit/test_managers.py
@@ -6,7 +6,10 @@ from user.models import User
 class TestUserManager(TestCase):
     def test__create_user(self) -> None:
         user: User = User.objects._create_user(
-            email="mail@mail.com", password="password", first_name="firstname", last_name="lastname"
+            email="mail@mail.com",
+            password="password",
+            first_name="firstname",
+            last_name="lastname",
         )
         user_created: User = User.objects.first()  # type: ignore
         self.assertEqual(user, user_created)
@@ -17,7 +20,11 @@ class TestUserManager(TestCase):
 
     def test_create_user(self) -> None:
         user: User = User.objects.create_user(
-            email="mail@mail.com", password="password", first_name="firstname", last_name="lastname")
+            email="mail@mail.com",
+            password="password",
+            first_name="firstname",
+            last_name="lastname",
+        )
         user_created: User = User.objects.first()
         self.assertEqual(user, user_created)
         self.assertEqual("mail@mail.com", user_created.email)
@@ -25,11 +32,23 @@ class TestUserManager(TestCase):
         self.assertEqual("firstname", user_created.first_name)
         self.assertEqual("lastname", user_created.last_name)
         self.assertFalse(user_created.is_superuser)
- 
+
+        with pytest.raises(ValueError) as ExceptionInfo:
+            user: User = User.objects.create_user(
+                email="",
+                password="password",
+                first_name="firstname",
+                last_name="lastname",
+            )
+        ExceptionInfo.match(r"The given email must be set.")
 
     def test_create_superuser(self) -> None:
         user: User = User.objects.create_superuser(
-            email="mail@mail.com", password="password", first_name="firstname", last_name="lastname")
+            email="mail@mail.com",
+            password="password",
+            first_name="firstname",
+            last_name="lastname",
+        )
         user_created: User = User.objects.first()
         self.assertEqual(user, user_created)
         self.assertEqual("mail@mail.com", user_created.email)
@@ -37,3 +56,22 @@ class TestUserManager(TestCase):
         self.assertEqual("firstname", user_created.first_name)
         self.assertEqual("lastname", user_created.last_name)
         self.assertTrue(user_created.is_superuser)
+
+        with pytest.raises(ValueError) as ExceptionInfo:
+            user: User = User.objects.create_superuser(
+                email="",
+                password="password",
+                first_name="firstname",
+                last_name="lastname",
+            )
+        ExceptionInfo.match(r"The given email must be set.")
+
+        with pytest.raises(ValueError) as ExceptionInfo:
+            user: User = User.objects.create_superuser(
+                email="mail@mail.com",
+                password="password",
+                first_name="firstname",
+                last_name="lastname",
+                is_superuser=False,
+            )
+        ExceptionInfo.match(r"Superuser must have is_superuser=True.")
