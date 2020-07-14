6ee96ad964dfdec1bc906846929fc55fab62dc6a
Ango <ango.gomes@gmail.com>
2020-07-14 23:11:35 +0200

    Added favorite save button display management

--- product/views.py
+++ product/views.py
@@ -1,6 +1,7 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
-from .models import Product, Category
 from django.views.generic import View
+from .models import Product, Category
 from .views import Product
 from substitute.models import Substitute
 
@@ -23,6 +24,11 @@ class SubstituteIndexView(View):
     def get(self, request, code_product):
         product = Product.objects.get(code=code_product)
         substitutes = Product.get_substitute(code_product)
+        user_favorite_list = []
+        if request.user.is_authenticated:
+            user_favorites = Substitute.get_favorite(request.user)
+            for favorite in user_favorites:
+                user_favorite_list.append(favorite.substitute_id)
         return render(
             request,
             "product/substitutes_index.html",
@@ -30,6 +36,7 @@ class SubstituteIndexView(View):
                 "product_code": code_product,
                 "product_name": product.name,
                 "substitutes": substitutes,
+                "favorites": user_favorite_list,
             },
         )
 
@@ -38,4 +45,5 @@ class SubstituteIndexView(View):
         product = Product.objects.filter(code=request.POST.get("product")).first()
         substitute = Product.objects.filter(code=request.POST.get("substitute")).first()
         Substitute.add_favorite(user, product, substitute)
-        return render(request, "app/home.html", {"favori_added": "success",},)
+        return JsonResponse({"favori_added": "success"})
+        # return render(request, "app/home.html", {"favori_added": "success",},)
